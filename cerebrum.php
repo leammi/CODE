90f8bbdaadadc3cc20326b2594d8ea0ef74376bf
leammi <45175321+leammi@users.noreply.github.com>
2018-12-04 08:42:36 +0100

    php

---
+++ cerebrum.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Cerebrum</title>
+    <link rel="stylesheet" href="reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Fredoka+One|Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body> 
+    <header>
+    <img class="logo" src="logo.svg" alt="logo" />
+    <script src="midnight.jquery.min.js"></script>
+    <nav class="fixed">
+        <div id="title">
+            <h1>Cerebrum</h1>
+            <h2>Le cerveau un labyrinthe complexe.</h2>
+        </div>
+    <img class="imgcerveau" src="cerveau-nav.png" alt="cerveau" />
+        <ul>
+            <li><a href="#">Introduction</a></li>
+            <li><a href="#">Musique</a></li>
+            <li><a href="#">Drogue</a></li>
+            <li><a href="#">Effet d'optique</a></li>
+        </ul>
+    </nav>
+    </header>
+    <section class="intro">
+        <h2>Introduction</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et fermentum dui. Ut orci quam, ornare sed lorem sed, hendrerit auctor dolor. Nulla viverra, nibh quis ultrices malesuada, ligula ipsum vulputate diam, aliquam egestas nibh ante vel dui. Sed in tellus interdum eros vulputate placerat sed non enim. Pellentesque eget justo porttitor urna dictum fermentum sit amet sed mauris. Praesent molestie vestibulum erat ac rhoncus. Aenean nunc risus, accumsan nec ipsum et, convallis sollicitudin dui. Proin dictum quam a semper malesuada. Etiam porta sit amet risus quis porta. Nulla facilisi. Cras at interdum ante. Ut gravida pharetra ligula vitae malesuada. </p>
+        
+    </section>
+    <section class="Musique">
+        <h2 id="titre_musique">La musique, oublier la fatigue et libérer la dopamine</h2>
+		<div class="conteneurmusique">
+		<div class="textemusique">
+        <p>La musique est un loisir très présent chez les jeunes grâce à la disponibilité mise en place des plateformes de streaming comme deezer, spotify, Youtube et SoundCloud. Cet intérêt pour un thème musical ou même la musique en général peut venir de sa famille, de ses amis ou de sa propre initiative.
+        </br>
+        Ce loisir avait déjà pris de l’ampleur grâce aux technologies d’enregistrement et de diffusion de son, le microphone, le vinyle, walkman, mp3, la radio, etc… 
+        </br>
+		<br></br>
+        Cette fameuse musique que nous écoutons tous les jours à de multiples effets sur le cerveau, elle peut faire oublier l’état de fatigue, réduire l’hypertension artérielle sous certaines conditions et devient l’élément principal de certains sportif pour l'entraînement, permettant de meilleures performances et influençant la perception d’effort.
+        </br>
+        “Il est aussi avéré que la musique agit sur la zone cérébrale des émotions et si jamais un morceau vous plait, va libérer de la dopamine au même titre qu’un sport extrême ou du chocolat.”  à mettre avec le bonhomme le saviez vous
+        </br>
+        Pour finir le sommeil, la relaxation et la créativité peuvent être impacter de manière positive par de la musique, une musique douce instrumentale ou chanté diminue l’état d’anxiété, améliore l’humeur et facilite l’endormissement.
+        </br>
+		<br></br>
+        Cet article est basé sur les études de la Southern Connecticut State University, la psychologue sportif Costas Karageorghis, de l’Université Brunel, au Royaume-Uni, des neuroscientifiques Den'etsu Sutoo et Kayo Akiyama, de l'université de Tsukuba au Japon et par l’article de Pascale Héon, “la musique et ses effets sur le cerveau”.</p>
+		</div>
+		<div class="imgmusique">
+        <img class="img_musique" src="musique.jpg" alt="photo de la musique">
+		</div>
+		</div>
+    </section>
+    <section class="drogue">
+        <h2>Les effets de la drogue dans le cerveau</h2>
+        <p>Tous produits qui déclenchent une dépendance chez l’Homme est considéré comme une drogue. Comme l’alcool, le tabac, les médicaments psychoactifs, la cocaïne, l’exctasy, l’héroine ou bien encore d’autres drogues. Chacune de ces substances produisent un déclenchement de dopamine relâché dans le cerveau, le circuit de la récompense. 
+        Chacune de ces drogues auront des effets différents sur le cerveau, mais elles vont simuler la substance produite naturellement par l’organisme pour se fixer à la place de celle-ci sur des récepteurs bien spécifiques.
+        On peut identifier 3 modes d’actions de ces différentes drogues : <ol>
+        <li>-   Elles imitent les neuromédiateurs naturels et se substituent à eux dans les récepteurs comme la morphine ou la nicotine.</li>
+        <li>-   Elles peuvent augmenter la sécrétion de neuromédiateurs naturels comme la cocaïne ou l’ectasy.</li>
+        <li>-   Ou elles peuvent bloquer les neuromédiateurs naturels comme l’alcool</li></ol></br>
+        Malgré tous ces modes d’actions différents, lorsque l’on consomme une drogue, elle augmente la libération de dopamine. La récompense est traduite par une sensation positive, soit une sensation de plaisir que l’être humaine ressent de manière naturelle. Comme l’action de se nourrir ou d’effectuer un acte sexuel. Cependant, lorsqu’il consomme cette drogue, il utilise le même circuit de récompense reproduisant une sensation de plaisir, mais une fausse récompense qui n’est pas bénéfique pour l’individu car elle détruit l’équilibre naturelle de ce circuit. </br>
+        Cette augmentation non naturelle de dopamine dans le cerveau peut induire une diminution du nombre de récepteurs à dopamine, donc en absence de la drogue, la quantité de dopamine redevenue normale ne sera donc plus suffisante pour recréer cette sensation de plaisir. </br>
+        D’autre part, lorsque l’on montre une simple image ou souvenir à une personne ayant déjà consommé une drogue, certaines zones du cerveau vont s’activer. Ces zones sont le circuit de la récompense qui va stimuler les neurones dopaminergiques, ce qui va créer une anticipation du plaisir et va repousser la personne à consommer à nouveau pour atteindre cette sensation de bonheur. On voit donc ici à quel point la mémoire de l’environnement de la prise de drogue est ancrée dans l’inconscient d’une personne dépendante.</p>
+    </section>
+    <section class="effet" data-midnight="white">
+        <h2>Les effets d'optique</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et fermentum dui. Ut orci quam, ornare sed lorem sed, hendrerit auctor dolor. Nulla viverra, nibh quis ultrices malesuada, ligula ipsum vulputate diam, aliquam egestas nibh ante vel dui. Sed in tellus interdum eros vulputate placerat sed non enim. Pellentesque eget justo porttitor urna dictum fermentum sit amet sed mauris. Praesent molestie vestibulum erat ac rhoncus. Aenean nunc risus, accumsan nec ipsum et, convallis sollicitudin dui. Proin dictum quam a semper malesuada. Etiam porta sit amet risus quis porta. Nulla facilisi. Cras at interdum ante. Ut gravida pharetra ligula vitae malesuada. </p>
+    <img class="cerveaueffet" src="perso logo droite.svg" alt="logo cerveau">
+    </section>
+</body>
